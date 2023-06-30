c181063147f3ae1b76e34ca2a5a6691653759825
Craig Olson <craigdolson@gmail.com>
2023-06-30 17:09:54 -0500

    fixing paths

--- home-manager/apps/kitty.nix
+++ home-manager/apps/kitty.nix
@@ -8,7 +8,7 @@
   };
 	programs.kitty = {
 	extraConfig = ''
-	include home-manager/dotfiles/kitty/kitty-themes/themes/Dracula.conf
+	include ~/nixos-laptop/home-manager/dotfiles/kitty/kitty-themes/themes/Dracula.conf
 	confirm_os_window_close 0
         background_opacity 0.8
         dynamic_background_opacity yes
