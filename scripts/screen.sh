5de4630bb166ca24e1121e72fefb7a92029cc2ff
Craig Olson <craigdolson@gmail.com>
2023-06-30 16:57:23 -0500

    seeding new repo

---
+++ scripts/screen.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output eDP-1 --mode 3840x2160 --pos 2560x0 --rotate normal --output HDMI-1 --off --output DP-1 --primary --mode 2560x1440 --pos 0x310 --rotate normal --output HDMI-2 --off
